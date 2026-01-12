be4c9f422e835048a24f3c06a7c6e27a7a445bb7
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2026-01-12 18:42:44 +0000

    Remove the `exclude_planes` feature to simplify the preprocessing pipeline. Users are now expected to filter their input data before passing it to pciSeq.
    (cherry picked from commit d6539bd9390cfa8ba860d91c4d8e0a0fb109a081)

--- pciSeq/src/validation/config.py
+++ pciSeq/src/validation/config.py
@@ -29,7 +29,6 @@ _TYPE_SPECS = {
     "cell_type_prior": str,
     "cell_type_weights": (type(None), dict),
     "voxel_size": list,
-    "exclude_planes": (type(None), list),
     "is3D": (type(None), bool),
     "remove_flat_cells": bool,
     "realtime_viewer": bool,
@@ -164,7 +163,6 @@ class Config(dict):
         """
 
         self["is3D"] = self._detect_3d(coo)
-        self["exclude_planes"] = self["exclude_planes"] or []
 
     def _detect_3d(self, coo) -> bool:
         """
