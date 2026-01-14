3e8d5e8a59aed6137236b9ee6200f7dfa9599b61
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2026-01-14 11:05:44 +0000

    set rTheta to 1.0 (from 25.0)

--- pciSeq/config.py
+++ pciSeq/config.py
@@ -141,7 +141,7 @@ DEFAULT = {
     "remove_flat_cells": True,
     # Runtime attribute (automatically set during execution)
     "is3D": None,
-    "rTheta": 25.0,
+    "rTheta": 1.0,
     # *******************************************************************************
     # Realtime viewer (optional visualization feature)
     # *******************************************************************************
